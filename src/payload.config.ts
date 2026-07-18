5b8ab1e8687afbf146dd8c5c40cf1ef1e75703e5
Sai Htet Myat Naing <saihtetmyatnaing4@gmail.com>
2026-07-18 17:26:55 +0700

    Add ImageKit plugin configuration

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import imagekitPlugin from "payloadcms-plugin-imagekit"
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
@@ -30,5 +31,33 @@ export default buildConfig({
     },
   }),
   sharp,
-  plugins: [],
+  plugins: [
+    imagekitPlugin({
+      config: {
+        publicKey: process.env.IMAGEKIT_PUBLIC_KEY || "your_public_api_key",
+        privateKey: process.env.IMAGEKIT_PRIVATE_KEY || "your_private_api_key",
+        endpoint: process.env.IMAGEKIT_ENDPOINT || "https://ik.imagekit.io/your_imagekit_id/",
+      },
+      collections: {
+        media: {
+          uploadOption: {
+            folder: "some folder",
+            extensions: [
+              {
+                name: "aws-auto-tagging",
+                minConfidence: 80,
+                maxTags: 10,
+              },
+              {
+                name: "google-auto-tagging",
+                minConfidence: 70,
+                maxTags: 10,
+              },
+            ],
+          },
+          savedProperties: ["url", "AITags"],
+        },
+      },
+    }),
+  ],
 })
